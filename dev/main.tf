af7ef8bc379b6803b1f4dbed003dc578baf10c7f
David Charbonnier <contact@davidcharbonnier.fr>
2024-12-01 22:48:25 -0500

    Updating to v30.0.0

--- dev/main.tf
+++ dev/main.tf
@@ -17,7 +17,7 @@
 # tfdoc:file:description Project factory.
 
 module "projects" {
-  source = "git@github.com:GoogleCloudPlatform/cloud-foundation-fabric.git//blueprints/factories/project-factory?ref=v28.0.0"
+  source = "git@github.com:GoogleCloudPlatform/cloud-foundation-fabric.git//modules/project-factory?ref=v30.0.0"
   data_defaults = {
     billing_account = var.billing_account.id
     # more defaults are available, check the project factory variables
@@ -33,7 +33,7 @@ module "projects" {
   data_overrides = {
     prefix = "${var.prefix}-dev"
   }
-  factory_data_path = var.factory_data_path
+  factories_config = var.factories_config
 }
 
 
